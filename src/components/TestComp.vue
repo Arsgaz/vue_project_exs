2858c76a0ddb58571e9ff06a5fff93a22b3aae74
Arsgaz <petrov.ars.i.23@gmail.com>
2025-02-06 12:15:39 +0300

    Task 4

---
+++ src/components/TestComp.vue
@@ -0,0 +1,16 @@
+<script>
+	export default {
+		data() {
+		    return {
+                name: 'John',
+		        sur: 'Smit',
+		    }
+	    }
+	}
+</script>
+<template>
+	<div>
+		<p>{{ name }}</p>
+	    <p>{{ sur }}</p>
+	</div>
+</template>
