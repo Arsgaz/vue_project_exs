6159efcfe6321ec5c1f6f1a5ae28b63000d5c08c
Arsgaz <petrov.ars.i.23@gmail.com>
2025-02-06 12:39:25 +0300

    Task 9

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,14 +1,17 @@
 <script>
 	export default {
 		data() {
-		    return {
-                obj: {x: 1, y: 2, z: 3},
-		    }
+		    
+	    },
+        methods: {
+            show: function() {
+		    alert(new Date());
 	    }
+	}
 	}
 </script>
 <template>
 	<div>
-		{{ obj.x + obj.y + obj.z }}
+		{{ show() }}
 	</div>
 </template>
