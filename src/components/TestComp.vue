33c16b5a1c5554e9d11b439c63fa666762a7bb7c
Arsgaz <petrov.ars.i.23@gmail.com>
2025-02-06 12:31:54 +0300

    Task 8

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,17 +2,13 @@
 	export default {
 		data() {
 		    return {
-                arr: ['x', 'y', 'z'],
-                arr1: [1, 2, 3],
+                obj: {x: 1, y: 2, z: 3},
 		    }
 	    }
 	}
 </script>
 <template>
 	<div>
-		<br>{{ arr[0] }}
-        <br>{{ arr[1] }}
-        <br>{{ arr[2] }}
-        <br>{{  arr1[0] + arr1[1] + arr1[2] }}
+		{{ obj.x + obj.y + obj.z }}
 	</div>
 </template>
